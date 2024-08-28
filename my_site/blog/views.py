d0f05c8db69652ef9812f8f946c6a6a3b30d29bb
kila <luka.kilasonia.1@btu.edu.ge>
2024-08-29 03:56:25 +0400

    [blog] create urls and views

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def starting_page(request):
+    return None
+
+def posts(requet):
+    return None
+
+
+def post_detail(request):
+    pass
