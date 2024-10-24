e60af7defd4439606c63b43b1f207336c5ba72eb
kila <luka.kilasonia.1@btu.edu.ge>
2024-10-25 03:28:28 +0400

    add comment form in context object

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -2,9 +2,10 @@ from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
 from datetime import date
-from .models import Post
 from django.views.generic import ListView, DetailView
 
+from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -34,4 +35,5 @@ class SinglePostView(DetailView):
     def get_context_data(self, **kwargs):
         context =  super().get_context_data(**kwargs)
         context['post_tags'] = self.object.tags.all()
+        context['comment_form'] = CommentForm()
         return context
