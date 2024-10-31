b61679561bdd5292b03b6379cdba77ef08623427
kila <luka.kilasonia.1@btu.edu.ge>
2024-11-01 02:10:23 +0400

    add comment section into admin panel

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Author, Post, Tag
+from .models import Author, Post, Tag, Comment
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
@@ -8,6 +8,10 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'date', 'author')
     prepopulated_fields = {'slug': ('title',)}
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'post')
+
 admin.site.register(Author) 
 admin.site.register(Tag)
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
